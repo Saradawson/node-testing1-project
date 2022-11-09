094defc795e8325b46629ed0b37893e5b034b246
unknown <saradawsone@gmail.com>
2022-11-09 11:46:42 -0700

    counter tests working

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ function trimPropertiesMutation(obj) {
  * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
  */
 function findLargestInteger(integers) {
-  // ✨ implement
+  let result = integers[0].integer
+  for(let i = 1; i < integers.length; i++){
+    if(integers[i].integer > result){
+      result = integers[i].integer
+    }
+  }
+  return result
 }
 
 class Counter {
@@ -47,7 +53,7 @@ class Counter {
    * @param {number} initialNumber - the initial state of the count
    */
   constructor(initialNumber) {
-    // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -63,7 +69,11 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    if(this.count === 0){
+      return this.count = 0
+    }else{
+      return this.count--
+    }
   }
 }
 
@@ -72,7 +82,7 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.season = 'summer'
   }
 
   /**
@@ -88,7 +98,7 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -30,25 +30,42 @@ describe('[Exercise 2] trimPropertiesMutation', () => {
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    const input = [{ integer: 1 }, { integer: 3 }, { integer: 2 }]
+    const actual = utils.findLargestInteger(input)
+    expect(actual).toBe(3)
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
   let counter
-  // beforeEach(() => {
-  //   counter = new utils.Counter(3) // each test must start with a fresh couter
-  // })
-  // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  beforeEach(() => {
+    counter = new utils.Counter(3) // each test must start with a fresh couter
+  })
+  test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
+    expect(counter.countDown()).toBe(3)
+  })
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    counter.countDown()
+    expect(counter.countDown()).toBe(2)
+  })
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    counter.countDown()
+    expect(counter.countDown()).toBe(0)
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
   let seasons
-  // beforeEach(() => {
-  //   seasons = new utils.Seasons() // each test must start with fresh seasons
-  // })
-  // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
+  beforeEach(() => {
+    seasons = new utils.Seasons() // each test must start with fresh seasons
+  })
+  test('[9] the FIRST call of seasons.next returns "summer"', () => {
+    
+  })
   // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
   // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
   // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
